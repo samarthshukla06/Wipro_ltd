bb9392c2e3b4786460c5758487b83ac5327cec33
Samarth Shukla <samarth06shukla@gmail.com>
2022-07-01 18:53:51 +0530

    HTML version 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { UtilityComponent } from './utility/utility.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    UtilityComponent
   ],
   imports: [
     BrowserModule,
